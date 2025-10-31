b7496d72453cf11eb5f2a58f4918760283a542dc
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2025-10-31 16:52:53 +0100

    feat: consolidate code,add app or other functions type

--- infra/aws/serverless/zip_to_s3.sh
+++ infra/aws/serverless/zip_to_s3.sh
@@ -4,26 +4,43 @@ set -x
 
 source ./env.sh
 
-#cat ./function_list_cf.txt
+# Check if parameter is provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <functions_type>"
+  echo "functions_type: 'simple' or 'app'"
+  exit 1
+fi
 
-FUNCTION_LIST=$(cat ./function_list_cf.txt)
+FUNCTIONS_TYPE=${1:-"simple"}
+
+FUNCTION_LIST_SIMPLE="./function_list_cf.txt"
+FUNCTION_LIST_APP="./function_list_cf_app.txt"
+
+if [ "$FUNCTIONS_TYPE" == "app" ]; then
+  FUNCTION_LIST_FILE=$FUNCTION_LIST_APP
+else
+  FUNCTION_LIST_FILE=$FUNCTION_LIST_SIMPLE
+fi
+
+FUNCTION_LIST=$(cat $FUNCTION_LIST_FILE)
 
 echo "function list: $FUNCTION_LIST"
 
 TMP_DIR="tmp"
 mkdir -p $TMP_DIR
 
-
 cd $TMP_DIR || exit
 #zip lambda.zip lambda_function.py
 
-pwd
+CURRENT_DIR=$(pwd)
+#ls -l "$CURRENT_DIR"/../../../../
+
 
 while IFS= read -r FUNCTION_NAME; do
   echo "function1 : $FUNCTION_NAME"
   # Pomiń puste linie
   if [[ -z "$FUNCTION_NAME" ]]; then
-    echo  "Ignoring empty line"
+    echo "Ignoring empty line"
     continue
   fi
 
@@ -32,15 +49,15 @@ while IFS= read -r FUNCTION_NAME; do
   LAMBDA_NAME="${PROJECT_NAME}-${ENVIRONMENT}-${FUNCTION_NAME}"
   echo "function3: >$LAMBDA_NAME<"
 
-#  zip -r "${LAMBDA_NAME}.zip" "../lambdas/${FUNCTION_NAME}"
-# wrong
-#  (cd "../lambdas/${FUNCTION_NAME}" && zip -r "../../tmp/${LAMBDA_NAME}.zip" .)
-
   TEMP_FUNCTION_DIR="${LAMBDA_NAME}_temp"
   mkdir -p "${TEMP_FUNCTION_DIR}"
 
   # Skopiuj zawartość katalogu funkcji do tymczasowego katalogu
   cp -r "../lambdas/${FUNCTION_NAME}/"* "${TEMP_FUNCTION_DIR}/"
+  if [ "$FUNCTIONS_TYPE" == "app" ]; then
+    ls -l "$CURRENT_DIR"/../
+    cp -r "$CURRENT_DIR"/../../../../backend/library "${TEMP_FUNCTION_DIR}/"
+  fi
 
   # Spakuj zawartość tymczasowego katalogu
   cd ${TEMP_FUNCTION_DIR}
@@ -59,11 +76,11 @@ while IFS= read -r FUNCTION_NAME; do
 
   # Wysyłanie pliku zip na S3
   aws s3 cp "${LAMBDA_NAME}.zip" "s3://${AWS_S3_BUCKET_NAME}/${LAMBDA_NAME}.zip"
-
   echo "Uploaded ${LAMBDA_NAME}.zip to S3"
 
-done <<< "$FUNCTION_LIST"
+  aws lambda update-function-code --function-name ${LAMBDA_NAME}  --zip-file fileb://"${LAMBDA_NAME}.zip" --profile ${PROFILE}
+
+done <<<"$FUNCTION_LIST"
 
 echo "Exit Code: 0"
 exit 0
-
